f3184783f137a6017df595cc6b088754824b0b01
Guilherme Vitor <guilhermevitor01_@outlook.com>
2023-10-03 11:18:50 -0300

    testando mira com o movimento

--- Coldcall-Moscow/Coldcall-Moscow/Coldcall-Moscow.cpp
+++ Coldcall-Moscow/Coldcall-Moscow/Coldcall-Moscow.cpp
@@ -1,25 +1,32 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/keyboard.h>
+#include <allegro5/mouse_cursor.h>
+#include <iostream>
 
 int main() {
 
     al_init();
     al_init_primitives_addon();
     al_install_keyboard();
+    al_install_mouse();
 
     ALLEGRO_DISPLAY* display = al_create_display(640, 480);
     al_set_window_position(display, 200, 200);
 
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
 
+    ALLEGRO_COLOR corMira = al_map_rgb(255, 255, 255);
+
     ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue();
     al_register_event_source(event_queue, al_get_display_event_source(display));
     al_register_event_source(event_queue, al_get_timer_event_source(timer));
     al_register_event_source(event_queue, al_get_keyboard_event_source());
+    al_register_event_source(event_queue, al_get_mouse_event_source());
     al_start_timer(timer);
 
     int pos_x = 0, pos_y = 0;
+    int mouseX = 0, mouseY = 0;
 
     while (true) {
         ALLEGRO_EVENT event;
@@ -40,10 +47,24 @@ int main() {
             pos_y += 10;
         }
 
-        al_clear_to_color(al_map_rgb(255, 255, 255));
+        if (event.type == ALLEGRO_EVENT_MOUSE_AXES) {
+            mouseX = event.mouse.x;
+            mouseY = event.mouse.y;
+
+        }
+
+        printf("%d     %d", mouseX, mouseY);
+
+        al_draw_filled_rectangle(mouseX - 1, mouseY - 2, mouseX + 2, mouseY - 15, al_map_rgb(255, 0, 0)); //Miras Verticais
+        al_draw_filled_rectangle(mouseX - 1, mouseY + 2, mouseX + 2, mouseY + 15, al_map_rgb(255, 0, 0));
+
+        al_draw_filled_rectangle(mouseX + 2, mouseY - 1, mouseX + 15, mouseY + 2, al_map_rgb(255, 0, 0)); //Miras Horizontais
+        al_draw_filled_rectangle(mouseX - 2, mouseY - 1, mouseX - 15, mouseY + 2, al_map_rgb(255, 0, 0));
 
         al_draw_filled_rectangle(0 + pos_x, 0 + pos_y, 20 + pos_x, 20 + pos_y, al_map_rgb(255, 0, 0));
+
         al_flip_display();
+        al_clear_to_color(al_map_rgb(255, 255, 255));
     }
 
     al_destroy_display(display);
